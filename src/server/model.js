0e9e5544838dfc7a862e840d553a42d22ad99888
Akshay Gupta <akshaygrkesh@gmail.com>
2018-12-30 15:29:53 +0530

    Changed response data format for queries

--- src/server/model.js
+++ src/server/model.js
@@ -41,41 +41,6 @@ const Item = mongoose.model('Item', Items)
 const Order = mongoose.model('Order', Orders)
 const User = mongoose.model('User', Users)
 
-exports.getItems = async function () {
-  return Item.find()
-}
-
-exports.getCart = async function (userId) {
-  let res = await User.findById(userId).populate('cart')
-  return res.cart
-}
-
-exports.getAddresses = async function (userId) {
-  let res = await User.findById(userId)
-  return res.addresses
-}
-
-exports.addToCart = async function (userId, item) {
-  let user = await User.findById(userId)
-  user.cart.push(item)
-  let res = await user.save()
-  return res.cart
-}
-
-exports.removeFromCart = async function (userId, item) {
-  let user = await User.findById(userId)
-  user.cart.splice(user.cart.indexOf(item), 1)
-  let res = await user.save()
-  return res.cart
-}
-
-exports.addAddress = async function (userId, address) {
-  let user = await User.findById(userId)
-  user.addresses.push(address)
-  let res = await user.save()
-  return res.addresses
-}
-
 function costOfCart (cart) {
   let cost = 0
   for (let item of cart) {
@@ -84,10 +49,45 @@ function costOfCart (cart) {
   return cost
 }
 
+exports.getItems = async function () {
+  return { menu: Item.find() }
+}
+
+exports.getCart = async function (userId) {
+  let res = await User.findById(userId).populate('cart')
+  return { cart: res.cart, total: costOfCart(res.cart) }
+}
+
+exports.getAddresses = async function (userId) {
+  let res = await User.findById(userId)
+  return { addresses: res.addresses }
+}
+
+exports.addToCart = async function (userId, item) {
+  let user = await User.findById(userId)
+  user.cart.push(item)
+  let res = await user.save().populate('cart')
+  return { cart: res.cart, total: costOfCart(res.cart) }
+}
+
+exports.removeFromCart = async function (userId, item) {
+  let user = await User.findById(userId)
+  user.cart.splice(user.cart.indexOf(item), 1)
+  let res = await user.save().populate('cart')
+  return { cart: res.cart, total: costOfCart(res.cart) }
+}
+
+exports.addAddress = async function (userId, address) {
+  let user = await User.findById(userId)
+  user.addresses.push(address)
+  let res = await user.save()
+  return { addresses: res.addresses }
+}
+
 exports.submitOrder = async function (userId, address) {
   let user = await User.findById(userId).populate('cart')
   let price = costOfCart(user.cart)
-  let order = new Order({ items: user.cart, date: Date.now(), price: price, address: address })
+  let order = new Order({ items: user.cart, date: Date.now(), total: price, address: address })
   user.cart = []
   user.currentOrders.push(order)
   return user.save()
