78202e81da79fb7766b61c74b3e815cd86411474
Akshay Gupta <akshaygrkesh@gmail.com>
2019-01-09 15:44:52 +0530

    Many more menu items

--- src/server/initializeDB.js
+++ src/server/initializeDB.js
@@ -52,6 +52,22 @@ async function setupDummyData () {
     restaurant: res.id
   }, 'Beverages')
   await addItem({
+    name: 'Heineken',
+    description: 'Probably the best beer in the world',
+    price: 450,
+    img: 'https://www.underconsideration.com/brandnew/archives/heineken_00_hero_shot_02.jpg',
+    available: true,
+    restaurant: res.id
+  }, 'Beverages')
+  await addItem({
+    name: 'Ramune',
+    description: 'Finely aged for the better part of a decade',
+    price: 1073,
+    img: 'https://www.domechan.com/shop/1424-thickbox_default/ramune-working-lemonade-japanese-taste-pineapple-200-ml.jpg',
+    available: true,
+    restaurant: res.id
+  }, 'Beverages')
+  await addItem({
     name: "Maulik's Chocolate",
     description: '100% Cocoa',
     price: 150,
@@ -60,8 +76,80 @@ async function setupDummyData () {
     restaurant: res.id
   }, 'Snacks')
   await addItem({
+    name: 'Choco Cornet',
+    description: 'Which side will YOU eat it from?',
+    price: 125,
+    img: 'https://images.japancentre.com/images/pics/12217/large/3566-chocolate-cornet-side.jpg?1469571297',
+    available: true,
+    restaurant: res.id
+  }, 'Snacks')
+  await addItem({
+    name: 'Microwaved Banana',
+    description: 'Delivered from the future just for you',
+    price: 25,
+    img: 'https://img-global.cpcdn.com/001_recipes/5325406057005056/640x640sq70/photo.jpg',
+    available: true,
+    restaurant: res.id
+  }, 'Snacks')
+  await addItem({
+    name: 'Soy Buttered Potatoes',
+    description: '',
+    price: 50,
+    img: 'http://justhungry.com/files/images/shinjagashouyubutter.jpg',
+    available: true,
+    restaurant: res.id
+  }, 'Snacks')
+  await addItem({
+    name: 'Cheese Lemon Custard Chiffon Pie',
+    description: 'Now with separate custard cream and cheese lemon chiffon layers!',
+    price: 225,
+    img: 'https://www.kcet.org/sites/kl/files/atoms/article_atoms/www.kcet.org/living/food/assets/images/lemonchiffonpie.png',
+    available: true,
+    restaurant: res.id
+  }, 'Dessert')
+  await addItem({
+    name: 'Tomato Sorbet',
+    description: 'Made with leftover fresh garden tomatoes',
+    price: 125,
+    img: 'http://tendingmygarden.com/wp-content/uploads/2010/08/L1690384tomsherbet-in-small-crystal.png',
+    available: true,
+    restaurant: res.id
+  }, 'Dessert')
+  await addItem({
+    name: 'The Cake',
+    description: '...is a lie',
+    price: 750,
+    img: 'https://i.kym-cdn.com/photos/images/original/000/115/357/portal-cake.jpg',
+    available: false,
+    restaurant: res.id
+  }, 'Dessert')
+  await addItem({
+    name: 'Herring and Pumpkin Pie',
+    description: "Grandma's secret recipe",
+    price: 650,
+    img: 'http://comicpoplibrary.com/wp-content/uploads/2011/06/herring-and-pumpkin-pie.jpg',
+    available: false,
+    restaurant: res.id
+  }, 'Dessert')
+  await addItem({
+    name: 'Omurice',
+    description: 'Omelette rice with ketchup. Made with love!',
+    price: 100,
+    img: 'https://sociorocketnewsen.files.wordpress.com/2014/06/kr-7.png',
+    available: true,
+    restaurant: res.id
+  }, 'Main Courses')
+  await addItem({
+    name: 'Chocolate Curry Rice',
+    description: 'Made with beloved turmeric, green jalapenos, cinnamon & cardamom, impossible paprika, green coriander, and now garam masala. We present to you delicious curry!',
+    price: 300,
+    img: 'https://japanesecurry.weebly.com/uploads/1/4/6/5/14652372/5502478_orig.jpg?0',
+    available: true,
+    restaurant: res.id
+  }, 'Main Courses')
+  await addItem({
     name: 'Happy Pizza',
-    description: 'Not legal in all territories. Conditions apply.',
+    description: 'Not legal in all territories. Conditions apply. Happiness not guaranteed.',
     price: 200,
     img: 'http://2.bp.blogspot.com/_Vh8ATwGRVqo/SvACUfs2A-I/AAAAAAAAKVk/KHyl0oKRNRA/s320/happy+pizza.JPG',
     available: true,
